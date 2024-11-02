3e1bb54c37167c1e7de4006250e2ac080ebdf74c
Phohou <danliu031505@gmail.com>
2024-11-02 00:33:45 -0500

    Added getBalance, addOrder, removeOrder, and rebalance

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -3,13 +3,13 @@ import java.lang.Math;
 public class AVLTree extends BookOrder{
 	
 	public class Node{
-		int data;
+		int key;
 		Node left;
 		Node right;
 		int height;
 		
 		public Node(int data) {
-			this.data = data;
+			key = data;
 		}
 	}
 	
@@ -20,7 +20,7 @@ public class AVLTree extends BookOrder{
 	public void updateHeight(Node x) {
 		int leftChildHeight = height(x.left);
 		int rightChildHeight = height(x.right);
-		x.height = Math.max(leftChildHeight, rightChildHeight) +1;
+		x.height = Math.max(leftChildHeight, rightChildHeight)+1;
 	}
 	
 	public Node rightRotation(Node x) {
@@ -43,12 +43,49 @@ public class AVLTree extends BookOrder{
 		return rightChild;
 	}
 	
-	public void addOrder() {
-		
+	public int getBalance(Node x) {
+		if (x == null) {
+			return 0;
+		}
+		return height(x.left) - height(x.right);
 	}
 	
-	public void removeOrder() {
-		
+	public Node rebalance(Node x) {
+		int balanceFactor = getBalance(x);
+		  if (balanceFactor < -1) {
+		    if (getBalance(x.left) <= 0) {    
+		      x = rightRotation(x);
+		    } else {                                
+		      x.left = leftRotation(x.left);
+		      x = rightRotation(x);
+		    }
+		  }
+
+
+		  if (balanceFactor > 1) {
+		    if (getBalance(x.right) >= 0) {    
+		      x = leftRotation(x);
+		    } else {                                 
+		      x.right = rightRotation(x.right);
+		      x = leftRotation(x);
+		    }
+		  }
+		  return x;
+	}
+	
+	public Node addOrder(Node x, int key) {
+		x = addOrder(x, key);
+		updateHeight(x);
+		return rebalance(x);
+    }
+	
+	public Node removeOrder(Node x, int key) {
+		x = removeOrder(x, key);
+		if (x == null) {
+			return x;
+		}
+		updateHeight(x);
+		return rebalance(x);
 	}
 	
 	public Node printTree(Node x) {
