99513478fe1e8cc4e7162e6fc5b917c8fd6d4587
Phohou <danliu031505@gmail.com>
2024-11-03 20:01:42 -0600

    Comments and interface
    Added comments to AVLTree methods and added an input interface for BookOrder

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -7,24 +7,26 @@ public class AVLTree{
 	public int height;
 	public String title;
 
+	//class that sets up and holds the avl tree//
 	public AVLTree() {
 		  int key;
 		  AVLTree left;
 		  AVLTree right;
 		  int height;
 		  String title;
-
-		  
 		}
-
+	//constructor for when making a node with a key
 		public AVLTree(int key) {
 		    this.key = key;
 		  }
-
+	/*method that finds the height of a node, returns -1 if the node is null and uses the built in .height command 
+	for existing nodes*/
 	public int height(AVLTree x) {
 		return x != null ? x.height : -1;
 	}
 	
+	/* method used for updating height of the avl tree, no return value since all it does is update existing
+	 values*/
 	public void updateHeight(AVLTree x) {
 		int leftChildHeight;
 		int rightChildHeight;
@@ -35,11 +37,13 @@ public class AVLTree{
 		if(x.right == null) {
 			rightChildHeight = 0;
 		}
-		leftChildHeight = height(x.left);
-		rightChildHeight = height(x.right);
-		x.height = Math.max(leftChildHeight, rightChildHeight)+1;
+		leftChildHeight = height(x.left); //sets the left tree height
+		rightChildHeight = height(x.right); //sets the right tree height
+		x.height = Math.max(leftChildHeight, rightChildHeight)+1; /*returns the higher value of either the 
+												left or right subtree and adds 1 to account for the root*/
 	}
 	
+	/* method used for right rotations*/
 	public AVLTree rightRotation(AVLTree x) {
 		AVLTree leftChild = x.left;
 		AVLTree nodeTemp = leftChild.right;
@@ -49,7 +53,7 @@ public class AVLTree{
 		updateHeight(leftChild);
 		return leftChild;
 	}
-	
+	/*method used for left rotations*/
 	public AVLTree leftRotation(AVLTree x) {
 		AVLTree rightChild = x.right;
 		AVLTree nodeTemp = rightChild.left;
@@ -59,30 +63,31 @@ public class AVLTree{
 		updateHeight(rightChild);
 		return rightChild;
 	}
-	
+	/*method used to get the balance factor of a node*/
 	public int getBalance(AVLTree x) {
 		if (x == null) {
 			return 0;
 		}
-		return height(x.left) - height(x.right);
+		return height(x.left) - height(x.right); //subtracts the height of the left subtree from the right to get bf
 	}
 	
+	/*method used for balancing of the avl tree*/
 	public AVLTree rebalance(AVLTree x) {
 		int balanceFactor = getBalance(x);
-		  if (balanceFactor < -1) {
-		    if (getBalance(x.left) <= 0) {    
+		  if (balanceFactor < -1) {  // >-1 balance factor calls for a rotation and tells us the tree is left heavy
+		    if (getBalance(x.left) <= 0) {    //if left side is balanced rotation right
 		      x = rightRotation(x);
-		    } else {                                
+		    } else {                          //otherwise do a left right rotation
 		      x.left = leftRotation(x.left);
 		      x = rightRotation(x);
 		    }
 		  }
 
 
-		  if (balanceFactor > 1) {
-		    if (getBalance(x.right) >= 0) {    
+		  if (balanceFactor > 1) {  // <1 balance factor calls for a rotation and tells us the tree is right heavy
+		    if (getBalance(x.right) >= 0) {    //if right side is balanced rotate left
 		      x = leftRotation(x);
-		    } else {                                 
+		    } else {                                 //otherwise do a right left rotation
 		      x.right = rightRotation(x.right);
 		      x = leftRotation(x);
 		    }
@@ -90,19 +95,21 @@ public class AVLTree{
 		  return x;
 	}
 	
+	/*method used for adding an order to the avl tree*/
 	public AVLTree addOrder(AVLTree x, int key, String title) {
-		x = addOrder(x, key, title);
-		updateHeight(x);
-		return rebalance(x);
+		x = addOrder(x, key, title); //creates a new node with the parameters
+		updateHeight(x); //updates the height of the tree
+		return rebalance(x); //rebalances the tree with the new given node
     }
 	
+	/*method used for removing an order from the tree*/
 	public AVLTree removeOrder(AVLTree x, int key) {
-		x = removeOrder(x, key);
-		if (x == null) {
+		x = removeOrder(x, key); //creates a node with the given
+		if (x == null) {  //if the node doesn't exist 
 			return x;
 		}
-		updateHeight(x);
-		return rebalance(x);
+		updateHeight(x);  //updates the height of the tree
+		return rebalance(x); //rebalances the tree
 	}
 	
 	public void printTree(AVLTree x) {
@@ -118,11 +125,11 @@ public class AVLTree{
 		return key;
 	}
 	
-	public AVLTree findOldest() {
-		
+	public AVLTree findOldest(AVLTree x) {
+		return x;
 	}
 	
-	public findLatest() {
-		
+	public AVLTree findLatest(AVLTree x) {
+		return x;
 	}
 }
--- src/BookOrder.java
+++ src/BookOrder.java
@@ -1,11 +1,60 @@
 import java.util.Scanner;
 
 public class BookOrder extends AVLTree{
+	static AVLTree tree = new AVLTree();
+	static boolean running = true;
+	static Scanner scan = new Scanner(System.in);
 	
-	public static void main(String argsp[]) {
-		AVLTree tree = new AVLTree();
-		Scanner scan = new Scanner(System.in);
-		int input = scan.nextInt();
+	//function that prints out commands//
+	public static void start() {
+		System.out.println("Welcome to the book ordering system please input a command:");
+		System.out.println("1: add");
+		System.out.println("2: remove");
+		System.out.println("3: print");
+		System.out.println("4: find");
 	}
 	
-}
+	public static void main(String argsp[]) {
+		while(running == true) {
+			start();
+			if (scan.hasNextInt()) {					//checks if user inputs an integer*/
+				int input = scan.nextInt();				//sets the int to a variable and checks if the int is valid
+				if (input == 1) {
+					System.out.println("add");
+				}
+				if (input == 2) {
+					System.out.println("remove");
+				}
+				if (input == 3) {
+					System.out.println("print");
+				}
+				if (input == 4) {
+					System.out.println("find");
+					}
+				else {
+					System.out.println("Please input a valid number 1-4 or command");  //figure out why this keeps running 
+				}
+				}
+			else if (scan.hasNextLine()) {				//same thing as before but with strings
+				String input = scan.nextLine();
+				if (input.equals("add")) {
+					System.out.println("add");
+				}
+				if (input.equals("remove")) {
+					System.out.println("remove");
+				}
+				if(input.equals("print")) {
+					System.out.println("print");
+				}
+				if(input.equals("find")) {
+					System.out.println("find");
+				}
+				else {
+					System.out.println("Please input a valid number 1-4 or command");
+				}
+			}
+
+				}
+			}
+			
+		}
